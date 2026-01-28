a265a729c3cfac8574453a7b7e2e500e18a60795
LJWsao <112007778+LJWsao@users.noreply.github.com>
2026-01-27 21:41:10 -0500

    Added SDA and SCL definitions car_entered_LoRa_v2.ino

--- ESP32/car_entered_LoRa_v2.ino
+++ ESP32/car_entered_LoRa_v2.ino
@@ -66,6 +66,9 @@ ImuState imuState = IMU_WAIT_UP;
 uint32_t imuStateStartMs = 0;
 uint32_t imuCooldownStartMs = 0;
 
+#define SDA 42
+#define SCL 41
+
 // ==================== HC-SR04 ====================
 #define TRIGGER_PIN   7
 #define ECHO_PIN      6
@@ -139,7 +142,7 @@ void setup() {
 
   showStatus("Booting...", "entry node");
 
-  Wire.begin();
+  Wire.begin(SDA, SCL);
   if (!mpu.begin()) {
     showStatus("MPU6050 FAIL");
     while (1) delay(100);
